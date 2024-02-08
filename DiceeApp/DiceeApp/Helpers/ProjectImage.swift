d232d5c64763da04d5084b0f324ac53e5962cb9f
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-08 20:39:04 +0300

    create enum with project images

---
+++ DiceeApp/DiceeApp/Helpers/ProjectImage.swift
@@ -0,0 +1,22 @@
+//
+//  ProjectImage.swift
+//  DiceeApp
+//
+//  Created by Nikolai Maksimov on 08.02.2024.
+//
+
+import Foundation
+
+enum DiceNumber: String, CaseIterable {
+    case one = "DiceOne"
+    case two = "DiceTwo"
+    case three = "DiceThree"
+    case four = "DiceFour"
+    case five = "DiceFive"
+    case six = "DiceSix"
+}
+
+enum DiceLogo: String {
+    case background = "GreenBackground"
+    case logo = "DiceeLogo"
+}
