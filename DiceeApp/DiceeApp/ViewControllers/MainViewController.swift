d83644394eda4e4626cc5f73bdd7911a08363a29
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-08 20:40:15 +0300

    refactoring, change project logic

--- DiceeApp/DiceeApp/ViewControllers/MainViewController.swift
+++ DiceeApp/DiceeApp/ViewControllers/MainViewController.swift
@@ -10,30 +10,17 @@ import UIKit
 final class MainViewController: UIViewController {
     
     // MARK: Private properties
-    private lazy var backgroundImageView = makeImageView(withImage: .background)
-    private lazy var diceeLogoImageView = makeImageView(withImage: .logo)
-    private lazy var leftDiceImageView = makeImageView(withDiceImage: .five)
-    private lazy var rightDiceImageView = makeImageView(withDiceImage: .one)
-    private lazy var rollButton = makeButton()
+    private lazy var backgroundImageView: UIImageView = .makeImageView(withImage: .background)
+    private let diceLogoView = DiceLogoView()
+    private let diceView = DiceView()
+    private let rollButtonView = RollButtonView()
+    private let stackView = UIStackView()
     
-    private enum DiceNumber: String, CaseIterable {
-        case one = "DiceOne"
-        case two = "DiceTwo"
-        case three = "DiceThree"
-        case four = "DiceFour"
-        case five = "DiceFive"
-        case six = "DiceSix"
-    }
-    
-    private enum DiceLogo: String {
-        case background = "GreenBackground"
-        case logo = "DiceeLogo"
-    }
-
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
+        updateUI()
     }
 }
 
@@ -41,42 +28,29 @@ final class MainViewController: UIViewController {
 extension MainViewController {
     
     private func commonInit() {
-        view.backgroundColor = .red
-        setupBackgroundImageView()
-        setupDiceeLogoConstraints()
-        setupLeftDiceImageViewConstraints()
-        setupRightDiceImageViewConstraints()
-        setupRollButtonConstraints()
+        setupBackgroundImageConstraints()
+        setupStackView()
+        setupStackViewConstraints()
     }
     
-    private func makeImageView(withDiceImage dice: DiceNumber? = nil, withImage image: DiceLogo? = nil) -> UIImageView {
-        let imageView = UIImageView()
-        imageView.image = dice != nil ? UIImage(named: dice?.rawValue ?? "")
-                                      : UIImage(named: image?.rawValue ?? "")
-        return imageView
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        [diceLogoView, diceView, rollButtonView].forEach { stackView.addArrangedSubview($0) }
     }
     
-    
-    private func makeButton() -> UIButton {
-        let button = UIButton()
-        button.configuration = .filled()
-        button.configuration?.title = "Roll"
-        button.configuration?.attributedTitle?.font = .systemFont(ofSize: 32)
-        button.addTarget(self, action: #selector(rollButtonPressed), for: .touchUpInside)
-        return button
-    }
-    
-    @objc
-    private func rollButtonPressed() {
-        leftDiceImageView.image = UIImage(named: DiceNumber.allCases.randomElement()?.rawValue ?? "")
-        rightDiceImageView.image = UIImage(named: DiceNumber.allCases.randomElement()?.rawValue ?? "")
+    private func updateUI() {
+        rollButtonView.completion = { [weak self] in
+            self?.diceView.update(leftDiceTitle: DiceNumber.allCases.randomElement()?.rawValue ?? "",
+                                  rightDiceTitle: DiceNumber.allCases.randomElement()?.rawValue ?? "")
+        }
     }
 }
 
 // MARK: - Constraints
 extension MainViewController {
     
-    private func setupBackgroundImageView() {
+    private func setupBackgroundImageConstraints() {
         view.addSubview(backgroundImageView)
         backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -87,46 +61,14 @@ extension MainViewController {
         ])
     }
     
-    private func setupDiceeLogoConstraints() {
-        view.addSubview(diceeLogoImageView)
-        diceeLogoImageView.translatesAutoresizingMaskIntoConstraints = false
+    private func setupStackViewConstraints() {
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            diceeLogoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            diceeLogoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            diceeLogoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            diceeLogoImageView.heightAnchor.constraint(equalToConstant: 250)
-        ])
-    }
-    
-    private func setupLeftDiceImageViewConstraints() {
-        view.addSubview(leftDiceImageView)
-        leftDiceImageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            leftDiceImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            leftDiceImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
-            leftDiceImageView.heightAnchor.constraint(equalToConstant: 125),
-            leftDiceImageView.widthAnchor.constraint(equalToConstant: 125),
-        ])
-    }
-    
-    private func setupRightDiceImageViewConstraints() {
-        view.addSubview(rightDiceImageView)
-        rightDiceImageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            rightDiceImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            rightDiceImageView.centerYAnchor.constraint(equalTo: leftDiceImageView.centerYAnchor),
-            rightDiceImageView.heightAnchor.constraint(equalToConstant: 125),
-            rightDiceImageView.widthAnchor.constraint(equalToConstant: 125),
-        ])
-    }
-    
-    private func setupRollButtonConstraints() {
-        view.addSubview(rollButton)
-        rollButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            rollButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            rollButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            rollButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
 }
