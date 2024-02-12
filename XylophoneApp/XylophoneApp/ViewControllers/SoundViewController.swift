fa684660e04a1bd6dd83ef518f863160f16a19f6
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-12 13:24:00 +0300

    setup project

---
+++ XylophoneApp/XylophoneApp/ViewControllers/SoundViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SoundViewController.swift
+//  XylophoneApp
+//
+//  Created by Nikolai Maksimov on 12.02.2024.
+//
+
+import UIKit
+
+final class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
