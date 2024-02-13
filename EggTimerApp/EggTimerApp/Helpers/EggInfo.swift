76375b966512540801c21a91a73f42722cab949d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-13 17:09:11 +0300

    add egg info enum

---
+++ EggTimerApp/EggTimerApp/Helpers/EggInfo.swift
@@ -0,0 +1,36 @@
+//
+//  ImageTitle.swift
+//  EggTimerApp
+//
+//  Created by Nikolai Maksimov on 13.02.2024.
+//
+
+import Foundation
+
+enum EggInfo: String, CaseIterable {
+    case soft = "Soft"
+    case medium = "Medium"
+    case hard = "Hard"
+    
+    var eggImageTitle: String {
+        switch self {
+        case .soft:
+            return "soft_egg"
+        case .medium:
+            return "medium_egg"
+        case .hard:
+            return "hard_egg"
+        }
+    }
+    
+    var eggTime: Int {
+        switch self {
+        case .soft:
+            return 3
+        case .medium:
+            return 4
+        case .hard:
+            return 7
+        }
+    }
+}
