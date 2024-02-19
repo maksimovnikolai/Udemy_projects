b4729b9728681d1843b9d7ebac6486abb2e69e5b
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-19 22:14:32 +0300

    configured user responses to questions
    answers to questions + counter of correct answers + progress bar

--- QuizzlerApp/QuizzlerApp/ViewControllers/QuizViewController.swift
+++ QuizzlerApp/QuizzlerApp/ViewControllers/QuizViewController.swift
@@ -9,10 +9,64 @@ import UIKit
 
 final class QuizViewController: UIViewController {
     
+    //MARK: Private Properties
+    private let quizzlerView = QuizzlerView()
+    private var quizBrain = QuizBrain()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+    // MARK: Life cycle
+    // Load View Method
+    override func loadView() {
+        view = quizzlerView
     }
     
+    // View Did Load Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commonInit()
+    }
+}
+
+//MARK: - Private Methods
+extension QuizViewController {
+    
+    private func commonInit() {
+        addTargetForAnswerButtons()
+        updateUI()
+    }
+
+    private func addTargetForAnswerButtons() {
+        quizzlerView.answerButtons.forEach {
+            $0.addTarget(self, action: #selector(answerButtonPressed), for: .touchUpInside)
+        }
+    }
+    
+    @objc
+    private func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnswer ?? "")
+        
+        sender.backgroundColor = userGotItRight ? .green : .red
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self,
+                             selector: #selector(updateUI),
+                             userInfo: nil,
+                             repeats: false)
+    }
+    
+    @objc
+    private func updateUI() {
+        let answerChoices = quizBrain.setTitle()
+        quizzlerView.choice1Button.setTitle(answerChoices[0], for: .normal)
+        quizzlerView.choice2Button.setTitle(answerChoices[1], for: .normal)
+        quizzlerView.choice3Button.setTitle(answerChoices[2], for: .normal)
+        
+        quizzlerView.questionLabel.text = quizBrain.getQuestionText()
+        quizzlerView.progressView.progress = quizBrain.getProgress()
+        quizzlerView.scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        quizzlerView.choice1Button.backgroundColor = .clear
+        quizzlerView.choice2Button.backgroundColor = .clear
+        quizzlerView.choice3Button.backgroundColor = .clear
+    }
 }
