7f63816b4e7e2e2baf37b398fbdfd4aeb32ddd80
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-20 18:45:07 +0300

    setup tab bar background image for all views

--- QuizzlerApp/QuizzlerApp/ViewControllers/MainTabBarController.swift
+++ QuizzlerApp/QuizzlerApp/ViewControllers/MainTabBarController.swift
@@ -9,22 +9,29 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
-    // MARK: - Private Properties
+    // MARK: Private Properties
     private lazy var vc1 = UINavigationController(rootViewController: setupQuizViewController())
+    private lazy var vc2 = UINavigationController(rootViewController: setupSettingsViewController())
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTabBar()
+        commonInit()
     }
 }
 
 // MARK: - Private Methods
 extension MainTabBarController {
     
+    private func commonInit() {
+        setupTabBar()
+        setupBackgroundImageViewConstraints()
+    }
+    
     private func setupTabBar() {
         tabBar.tintColor = .yellow
-        setViewControllers([vc1], animated: true)
+        tabBar.unselectedItemTintColor = .gray
+        setViewControllers([vc1, vc2], animated: true)
     }
     
     private func setupQuizViewController() -> UIViewController {
@@ -32,5 +39,34 @@ extension MainTabBarController {
         quizVC.tabBarItem.image = UIImage(systemName: "house")
         quizVC.title = "Quiz"
         return quizVC
+    }
+    
+    private func setupSettingsViewController() -> UIViewController {
+        let settingsVC = SettingsViewController()
+        settingsVC.tabBarItem.image = UIImage(systemName: "gear")
+        settingsVC.title = "Settings"
+        return settingsVC
+    }
+}
+
+// MARK: - Tab Bar Image
+extension MainTabBarController {
+    
+    private func makeImageView() -> UIImageView {
+        let imageView = UIImageView()
+         imageView.image = #imageLiteral(resourceName: "Background-Bubbles")
+         return imageView
+    }
+    
+    private func setupBackgroundImageViewConstraints() {
+        let imageView = makeImageView()
+        tabBar.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
