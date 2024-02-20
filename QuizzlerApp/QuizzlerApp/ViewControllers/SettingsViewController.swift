a5f4b7b0168f8100521ecefb4b2f40280c0918ce
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-20 18:42:48 +0300

    add settings view controller

---
+++ QuizzlerApp/QuizzlerApp/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  QuizzlerApp
+//
+//  Created by Nikolai Maksimov on 20.02.2024.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 37/255, green: 44/255, blue: 74/255, alpha: 1)
+    }
+}
