3a739863fae9b54f70227452b8e877c5c08ed30e
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-21 19:49:59 +0300

    add model

---
+++ BMICalculatorApp/BMICalculatorApp/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMICalculatorApp
+//
+//  Created by Nikolai Maksimov on 21.02.2024.
+//
+
+import Foundation
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: String
+}
