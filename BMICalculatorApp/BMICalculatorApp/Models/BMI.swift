b9bf625ca564a0b8b424bd2d5fd3a934f4c7577e
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-21 20:17:59 +0300

    add calculate brain

--- BMICalculatorApp/BMICalculatorApp/Models/BMI.swift
+++ BMICalculatorApp/BMICalculatorApp/Models/BMI.swift
@@ -5,10 +5,10 @@
 //  Created by Nikolai Maksimov on 21.02.2024.
 //
 
-import Foundation
+import UIKit
 
 struct BMI {
     let value: Float
     let advice: String
-    let color: String
+    let color: UIColor
 }
---
+++ BMICalculatorApp/BMICalculatorApp/Models/CalculateBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculateBrain.swift
+//  BMICalculatorApp
+//
+//  Created by Nikolai Maksimov on 21.02.2024.
+//
+
+import UIKit
+
+
+struct CalculateBrain {
+    
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .white
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: .blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .systemPink)
+        }
+    }
+}
